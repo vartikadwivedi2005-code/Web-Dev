67b6d8d8c07d4ff48bc29d8d9db4e8c632535a16
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-05-21 13:24:47 +0530

    merged new.js with index.js

---
+++ lect_41/index.js
@@ -0,0 +1,11 @@
+// import data from './new.js'
+// console.log(data);
+// import {a} from  './new.js'
+// console.log(data);
+
+import {data,sum} from './new.js'
+sum(5,6)
+console.log(data)
+
+
+
---
+++ lect_41/new.js
@@ -0,0 +1,9 @@
+// let a = 5
+
+// export default a=5
+
+let data="hello"
+function sum(a,b){
+    console.log(a+b)
+}
+export {data,sum}
